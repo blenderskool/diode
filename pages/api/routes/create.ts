2e0a8ee66c90e0ca2590b2753ab6f654ff9d00e3
blenderskool <akash.hamirwasia@gmail.com>
2021-11-14 12:13:47 +0530

    Add ApiRoute creation api endpoint

---
+++ pages/api/routes/create.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { nanoid } from 'nanoid';
+import { URL } from 'url';
+import prisma from '../../../lib/prisma';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    return res.status(405).send("Method not allowed");
+  }
+
+  const { name, method, projectId } = req.body;
+  const apiUrl = new URL(req.body.apiUrl);
+
+  // Handle multiple query params
+  const queryParams = [...apiUrl.searchParams];
+
+  const newRouteId = await prisma.apiRoute.create({
+    data: {
+      name,
+      method,
+      apiUrl: apiUrl.origin + apiUrl.pathname,
+      projectId,
+      id: nanoid(),
+      caching: {},
+      headers: [],
+      queryParams,
+      rateLimiting: {},
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  res.status(200).json(newRouteId);
+}
