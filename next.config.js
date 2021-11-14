ab4d917b5c59e2b9ac50a922446a59773572c1db
blenderskool <akash.hamirwasia@gmail.com>
2021-11-14 18:24:27 +0530

    Add landing page redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/projects',
+        permanent: false,
+      },
+    ]
+  },
 }
