a827709b1babf2eb8fedfc0801614331a996eaca
blenderskool <akash.hamirwasia@gmail.com>
2021-11-14 17:02:57 +0530

    Add project repo link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,16 @@
-import { Box, Link } from '@chakra-ui/react';
+import { Box, Link, Text } from '@chakra-ui/react';
 
 export default function Footer(props) {
   return (
     <Box mt="auto" textAlign="center" color="gray.600" {...props}>
-      Made in 🇮🇳 by <Link href="https://akashhamirwasia.com" fontWeight="500" color="green.500" isExternal>Akash Hamirwasia</Link>
+      <Text fontWeight="500">
+        Diode is open source on
+        {' '}
+        <Link href="https://github.com/blenderskool/diode" fontWeight="500" color="green.500" isExternal>GitHub</Link>
+      </Text>
+      <Text mt="1">
+        Made in 🇮🇳 by <Link href="https://akashhamirwasia.com" fontWeight="500" color="green.500" isExternal>Akash Hamirwasia</Link>
+      </Text>
     </Box>
   );
 }
