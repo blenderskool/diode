0767614aba50492fcb36d222fde00684088777ae
blenderskool <akash.hamirwasia@gmail.com>
2021-11-26 18:22:48 +0530

    Add mock deployment banner

---
+++ components/MockDeploymentBanner.tsx
@@ -0,0 +1,65 @@
+import {
+  Flex,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalCloseButton,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  Heading,
+  Text,
+  UnorderedList,
+  ListItem,
+  Code,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+
+export default function MockDeploymentBanner() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  if (process.env.NEXT_PUBLIC_MOCK_DEPLOYMENT !== "true") return null;
+
+  return (
+    <>
+      <Flex bg="red.100" py="1" color="red.600" fontWeight="semibold" fontSize="sm" alignItems="center" justifyContent="center">
+        This is a mock deployment, all features are still available in a limited capacity
+        <Button colorScheme="red" size="xs" ml="4" onClick={() => setModalOpen(true)}>
+          Read more
+        </Button>
+      </Flex>
+
+      <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+        <ModalOverlay />
+          <ModalContent p="2" pt="4">
+            <ModalHeader>
+              <Heading fontFamily="heading" fontWeight="700" fontSize="2xl">What is a mock deployment? 🧪</Heading>
+            </ModalHeader>
+
+            <ModalBody pb={8} pt={4}>
+              <Text fontSize="small" color="gray.500" fontWeight="500" mt="2">
+                Mock deployments are for <strong>testing and demonstration</strong> purposes. They are not intended for any serious or production use cases.
+              </Text>
+              <br />
+              <UnorderedList fontSize="small" color="gray.500" fontWeight="500">
+                <ListItem>
+                  In-memory Redis mock server is used.
+                </ListItem>
+                <ListItem>
+                  Data and functions state may not be persisted for longer durations. It can get cleared at any point.
+                </ListItem>
+              </UnorderedList>
+              <br />
+              <Text fontSize="small" color="gray.500" fontWeight="500">
+                To use Diode 🔌 in production mode, set environment variable:
+                <br />
+                <Code>NEXT_PUBLIC_MOCK_DEPLOYMENT=&quot;false&quot;</Code>
+              </Text>
+            </ModalBody>
+
+            <ModalCloseButton />
+          </ModalContent>
+      </Modal>
+    </>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 
 import Footer from '../components/Footer';
+import MockDeploymentBanner from '../components/MockDeploymentBanner';
 
 const theme = extendTheme({
   config: {
@@ -20,6 +21,7 @@ const theme = extendTheme({
 function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <MockDeploymentBanner />
       <Box bg="gray.100" minH="100vh">
         <Container shadow="base" bg="white" rounded="base" maxWidth="container.lg">
           <Flex direction="column" pt="16" pb="8" px="16" minH="100vh">
