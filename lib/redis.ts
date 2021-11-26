d39ce6f6b5b6b69fe675f107c932e08c78a6265e
blenderskool <akash.hamirwasia@gmail.com>
2021-11-26 18:22:28 +0530

    Add mock redis server in mock deployment mode

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,5 +1,11 @@
-import Redis from 'ioredis';
+import Redis, { Redis as RedisType } from 'ioredis';
+import RedisMock from 'ioredis-mock';
 
-const client = new Redis(process.env.REDIS_URL);
+let client: RedisType;
+if (process.env.NEXT_PUBLIC_MOCK_DEPLOYMENT === "true") {
+  client = new RedisMock();
+} else {
+  client = new Redis(process.env.REDIS_URL);
+}
 
 export default client;
