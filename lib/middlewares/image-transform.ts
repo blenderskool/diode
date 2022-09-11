5e000aafc02c44838fe5c969462542bfdc41aa5a
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2022-09-11 08:47:57 +0000

    Add `mask` parameter
    `'circle'` and `'ellipse'` masks have been added using
    compositing capabilities of sharp.

--- lib/middlewares/image-transform.ts
+++ lib/middlewares/image-transform.ts
@@ -4,8 +4,7 @@ import { getType } from 'mime';
 
 import { ApiRouteWithMiddlewares } from 'pages/api/v1/_types';
 
-export interface ImageTransformationOptions extends MiddlewareOptions {
-};
+export interface ImageTransformationOptions extends MiddlewareOptions {};
 
 const getValue = <T = string>(query: string | string[], parse: (val: string) => T, defaultVal: T = undefined): T => {
   if (Array.isArray(query)) {
@@ -18,19 +17,36 @@ const getValue = <T = string>(query: string | string[], parse: (val: string) => 
 const remap = (value: number, inMin: number, inMax: number, outMin: number, outMax: number) => 
   (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 
-
-export const imageTransformationReader = (apiRoute: ApiRouteWithMiddlewares) => (req: NextApiRequest, res: NextApiResponse, next) => {
-  const imageTransformation = apiRoute.imageTransformation as ImageTransformationOptions;
-  if (!imageTransformation.enabled) {
-    next();
-    return;
-  }
-
-  req.locals.url = getValue(req.query['diode-image'], String);
-  next();
+const MASKS = {
+  circle: (radius) => Buffer.from(
+    `<svg><circle cx="${radius}" cy="${radius}" r="${radius}"/></svg>`
+  ),
+  ellipse: (width: number, height: number) => Buffer.from(
+    `<svg><rect x="0" y="0" width="${width}" height="${height}" rx="${width/2}" ry="${height/2}"/></svg>`
+  ),
 }
 
-export function imageTransformation(apiRoute: ApiRouteWithMiddlewares) {
+const createMasks = (mask: keyof typeof MASKS, info: sharp.OutputInfo) => {
+  const masks = [];
+  switch(mask) {
+    case 'circle':
+      masks.push({
+        input: MASKS.circle(Math.min(info.width, info.height)/2),
+        blend: 'dest-in'
+      });
+      break;
+    case 'ellipse':
+      masks.push({
+        input: MASKS.ellipse(info.width, info.height),
+        blend: 'dest-in'
+      });
+      break;
+  }
+
+  return masks;
+}
+
+export function imageTransformation(apiRoute: ApiRouteWithMiddlewares, transforms: URLSearchParams) {
   return async (req: NextApiRequest, res: NextApiResponse, next) => {
     const imageTransformation = apiRoute.imageTransformation as ImageTransformationOptions;
     if (!imageTransformation.enabled) {
@@ -38,34 +54,45 @@ export function imageTransformation(apiRoute: ApiRouteWithMiddlewares) {
       return;
     }
 
-    const w = getValue(req.query.w, parseInt);
-    const h = getValue(req.query.h, parseInt);
-    const fit = getValue(req.query.fit, String, 'cover') as keyof FitEnum;
-    const format = getValue(req.query.fm, String, 'png') as keyof FormatEnum;
-    const quality = getValue(req.query.q, parseInt);
-    const colors = getValue(req.query.colorquant, parseInt);
-    const crop = getValue(req.query.crop, (s) => sharp.strategy[s], 'center');
-    const blur = getValue(req.query.blur, parseInt, 0);
-    const saturation = remap(getValue(req.query.sat, parseInt, 0), -100, 100, 0, 2);
-    const brightness = remap(getValue(req.query.bright, parseInt, 0), -100, 100, 0, 2);
-    const contrast = remap(getValue(req.query.cont, parseInt, 0), -100, 100, 0, 2);
-    const progressive = getValue(req.query.prog, Boolean, false);
+    const w = getValue(transforms.get('w'), parseInt);
+    const h = getValue(transforms.get('h'), parseInt);
+    const fit = getValue(transforms.get('fit'), String, 'cover') as keyof FitEnum;
+    const format = getValue(transforms.get('fm'), String, 'png') as keyof FormatEnum;
+    const quality = getValue(transforms.get('q'), parseInt);
+    const colors = getValue(transforms.get('colorquant'), parseInt);
+    const crop = getValue(transforms.get('crop'), (s) => sharp.strategy[s], 'center');
+    const blur = getValue(transforms.get('blur'), parseInt, 0);
+    const saturation = remap(getValue(transforms.get('sat'), parseInt, 0), -100, 100, 0, 2);
+    const brightness = remap(getValue(transforms.get('bright'), parseInt, 0), -100, 100, 0, 2);
+    const contrast = remap(getValue(transforms.get('cont'), parseInt, 0), -100, 100, 0, 2);
+    const progressive = getValue(transforms.get('prog'), Boolean, false);
+    const mask = getValue(transforms.get('mask'), String, null) as keyof typeof MASKS;
 
     const image = req.locals.result.data;
 
-    const output = await sharp(image)
+    /**
+     * First pass resizes the image so that the mask can use
+     * resized widths and heights.
+     */
+    const { data: resized, info } = await sharp(image)
       .resize({
         width: w,
         height: h,
         fit,
         position: crop,
       })
+      .toBuffer({ resolveWithObject: true });
+
+    const output = await sharp(resized)
       .blur(blur === 0 ? false : 1 + blur / 2)
       .modulate({
         brightness: brightness,
         saturation: saturation,
       })
       .linear(contrast, -(128 * contrast) + 128)
+      .composite([
+        ...createMasks(mask, info),
+      ])
       .toFormat(format, {
         quality,
         colors,
